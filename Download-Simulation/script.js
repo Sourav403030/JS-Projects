dc125cf0b7019d86e275f2e87757f475ad93036c
Sourav Sanjay <souravsanjay07@gmail.com>
2025-11-18 23:16:14 +0530

    download simulation project done

---
+++ Download-Simulation/script.js
@@ -0,0 +1,25 @@
+let p = document.querySelector("p");
+let inner = document.querySelector(".inner");
+let h1 = document.querySelector(".bottom h1");
+let button = document.querySelector(".bottom button");
+let span = document.querySelector("p span");
+
+let barProgress = 0;
+let randomProgress = Math.floor(50 + Math.random()*100);
+
+button.addEventListener("click", () => {
+  button.style.pointerEvents = "none";
+  button.style.opacity = "30%";
+  p.textContent = "Downloading...";
+  let percentage = setInterval(() => {
+    barProgress++;
+    h1.textContent = barProgress + "%";
+    inner.style.width = barProgress+"%";
+  }, randomProgress);
+
+  setTimeout(() => {
+    clearInterval(percentage);
+    p.innerText = `Your Download finished in ${randomProgress/10}s`
+
+  }, randomProgress*100);
+});
