70130b15a1db05077a69fb4f46a1af58c603972c
Sourav Sanjay <souravsanjay07@gmail.com>
2025-11-22 15:49:49 +0530

    Online piano task done

---
+++ Online-Piano/script.js
@@ -0,0 +1,462 @@
+let body = document.querySelector("body");
+let alphabet = document.querySelectorAll(".alphabet h1");
+let white = document.querySelectorAll(".white");
+let black = document.querySelectorAll(".black");
+
+let aud1 = new Audio("music/28.mp3");
+let aud2 = new Audio("music/29.mp3");
+let aud3 = new Audio("music/30.mp3");
+let aud4 = new Audio("music/31.mp3");
+let aud5 = new Audio("music/32.mp3");
+let aud6 = new Audio("music/33.mp3");
+let aud7 = new Audio("music/34.mp3");
+let aud8 = new Audio("music/35.mp3");
+let aud9 = new Audio("music/36.mp3");
+let aud10 = new Audio("music/37.mp3");
+let aud11 = new Audio("music/38.mp3");
+let aud12 = new Audio("music/39.mp3");
+let aud13 = new Audio("music/40.mp3");
+let aud14 = new Audio("music/41.mp3");
+let aud15 = new Audio("music/42.mp3");
+let aud16 = new Audio("music/43.mp3");
+let aud17 = new Audio("music/44.mp3");
+let aud18 = new Audio("music/45.mp3");
+let aud19 = new Audio("music/46.mp3");
+let aud20 = new Audio("music/47.mp3");
+let aud21 = new Audio("music/48.mp3");
+let aud22 = new Audio("music/49.mp3");
+let aud23 = new Audio("music/50.mp3");
+let aud24 = new Audio("music/51.mp3");
+let aud25 = new Audio("music/52.mp3");
+let aud26 = new Audio("music/53.mp3");
+let aud27 = new Audio("music/54.mp3");
+
+
+
+body.addEventListener("keydown", (evt)=>{
+    if(evt.code === "KeyA"){
+        white[0].classList.add("active");
+        aud1.play();
+        aud1.currentTime = 0;
+    }
+})
+
+body.addEventListener("keyup", (evt)=>{
+    if(evt.code === "KeyA"){
+        white[0].classList.remove("active");
+    }
+})
+
+
+body.addEventListener("keydown", (evt)=>{
+    if(evt.code === "KeyB"){
+        white[1].classList.add("active");
+        aud2.play();
+        aud2.currentTime = 0;
+    }
+})
+
+body.addEventListener("keyup", (evt)=>{
+    if(evt.code === "KeyB"){
+        white[1].classList.remove("active");
+    }
+})
+
+
+body.addEventListener("keydown", (evt)=>{
+    if(evt.code === "KeyC"){
+        white[2].classList.add("active");
+        aud3.play();
+        aud3.currentTime = 0;
+    }
+})
+
+body.addEventListener("keyup", (evt)=>{
+    if(evt.code === "KeyC"){
+        white[2].classList.remove("active");
+    }
+})
+
+
+body.addEventListener("keydown", (evt)=>{
+    if(evt.code === "KeyD"){
+        white[3].classList.add("active");
+        aud4.play();
+        aud4.currentTime = 0;
+    }
+})
+
+body.addEventListener("keyup", (evt)=>{
+    if(evt.code === "KeyD"){
+        white[3].classList.remove("active");
+    }
+})
+
+
+
+body.addEventListener("keydown", (evt)=>{
+    if(evt.code === "KeyE"){
+        white[4].classList.add("active");
+        aud5.play();
+        aud5.currentTime = 0;
+    }
+})
+
+body.addEventListener("keyup", (evt)=>{
+    if(evt.code === "KeyE"){
+        white[4].classList.remove("active");
+    }
+})
+
+
+
+body.addEventListener("keydown", (evt)=>{
+    if(evt.code === "KeyF"){
+        white[5].classList.add("active");
+        aud6.play();
+        aud6.currentTime = 0;
+    }
+})
+
+body.addEventListener("keyup", (evt)=>{
+    if(evt.code === "KeyF"){
+        white[5].classList.remove("active");
+    }
+})
+
+
+
+body.addEventListener("keydown", (evt)=>{
+    if(evt.code === "KeyG"){
+        white[6].classList.add("active");
+        aud7.play();
+        aud7.currentTime = 0;
+    }
+})
+
+body.addEventListener("keyup", (evt)=>{
+    if(evt.code === "KeyG"){
+        white[6].classList.remove("active");
+    }
+})
+
+
+
+body.addEventListener("keydown", (evt)=>{
+    if(evt.code === "KeyH"){
+        white[7].classList.add("active");
+        aud8.play();
+        aud8.currentTime = 0;
+    }
+})
+
+body.addEventListener("keyup", (evt)=>{
+    if(evt.code === "KeyH"){
+        white[7].classList.remove("active");
+    }
+})
+
+
+
+body.addEventListener("keydown", (evt)=>{
+    if(evt.code === "KeyI"){
+        white[8].classList.add("active");
+        aud9.play();
+        aud9.currentTime = 0;
+    }
+})
+
+body.addEventListener("keyup", (evt)=>{
+    if(evt.code === "KeyI"){
+        white[8].classList.remove("active");
+    }
+})
+
+
+
+body.addEventListener("keydown", (evt)=>{
+    if(evt.code === "KeyJ"){
+        white[9].classList.add("active");
+        aud10.play();
+        aud10.currentTime = 0;
+    }
+})
+
+body.addEventListener("keyup", (evt)=>{
+    if(evt.code === "KeyJ"){
+        white[9].classList.remove("active");
+    }
+})
+
+
+
+
+body.addEventListener("keydown", (evt)=>{
+    if(evt.code === "KeyK"){
+        white[10].classList.add("active");
+        aud11.play();
+        aud11.currentTime = 0;
+    }
+})
+
+body.addEventListener("keyup", (evt)=>{
+    if(evt.code === "KeyK"){
+        white[10].classList.remove("active");
+    }
+})
+
+
+
+
+body.addEventListener("keydown", (evt)=>{
+    if(evt.code === "KeyL"){
+        white[11].classList.add("active");
+        aud12.play();
+        aud12.currentTime = 0;
+    }
+})
+
+body.addEventListener("keyup", (evt)=>{
+    if(evt.code === "KeyL"){
+        white[11].classList.remove("active");
+    }
+})
+
+
+
+
+body.addEventListener("keydown", (evt)=>{
+    if(evt.code === "KeyM"){
+        white[12].classList.add("active");
+        aud13.play();
+        aud13.currentTime = 0;
+    }
+})
+
+body.addEventListener("keyup", (evt)=>{
+    if(evt.code === "KeyM"){
+        white[12].classList.remove("active");
+    }
+})
+
+
+
+
+body.addEventListener("keydown", (evt)=>{
+    if(evt.code === "KeyN"){
+        white[13].classList.add("active");
+        aud14.play();
+        aud14.currentTime = 0;
+    }
+})
+
+body.addEventListener("keyup", (evt)=>{
+    if(evt.code === "KeyN"){
+        white[13].classList.remove("active");
+    }
+})
+
+
+
+body.addEventListener("keydown", (evt)=>{
+    if(evt.code === "KeyO"){
+        white[14].classList.add("active");
+        aud15.play();
+        aud15.currentTime = 0;
+    }
+})
+
+body.addEventListener("keyup", (evt)=>{
+    if(evt.code === "KeyO"){
+        white[14].classList.remove("active");
+    }
+})
+
+
+
+body.addEventListener("keydown", (evt)=>{
+    if(evt.code === "KeyP"){
+        white[15].classList.add("active");
+        aud16.play();
+        aud16.currentTime = 0;
+    }
+})
+
+body.addEventListener("keyup", (evt)=>{
+    if(evt.code === "KeyP"){
+        white[15].classList.remove("active");
+    }
+})
+
+
+body.addEventListener("keydown", (evt)=>{
+    if(evt.code === "KeyQ"){
+        black[0].classList.add("active");
+        aud17.play();
+        aud17.currentTime = 0;
+    }
+})
+
+body.addEventListener("keyup", (evt)=>{
+    if(evt.code === "KeyQ"){
+        black[0].classList.remove("active");
+    }
+})
+
+
+body.addEventListener("keydown", (evt)=>{
+    if(evt.code === "KeyR"){
+        black[1].classList.add("active");
+        aud18.play();
+        aud18.currentTime = 0;
+    }
+})
+
+body.addEventListener("keyup", (evt)=>{
+    if(evt.code === "KeyR"){
+        black[1].classList.remove("active");
+    }
+})
+
+
+body.addEventListener("keydown", (evt)=>{
+    if(evt.code === "KeyS"){
+        black[2].classList.add("active");
+        aud19.play();
+        aud19.currentTime = 0;
+    }
+})
+
+body.addEventListener("keyup", (evt)=>{
+    if(evt.code === "KeyS"){
+        black[2].classList.remove("active");
+    }
+})
+
+
+body.addEventListener("keydown", (evt)=>{
+    if(evt.code === "KeyT"){
+        black[3].classList.add("active");
+        aud20.play();
+        aud20.currentTime = 0;
+    }
+})
+
+body.addEventListener("keyup", (evt)=>{
+    if(evt.code === "KeyT"){
+        black[3].classList.remove("active");
+    }
+})
+
+
+
+body.addEventListener("keydown", (evt)=>{
+    if(evt.code === "KeyU"){
+        black[4].classList.add("active");
+        aud21.play();
+        aud21.currentTime = 0;
+    }
+})
+
+body.addEventListener("keyup", (evt)=>{
+    if(evt.code === "KeyU"){
+        black[4].classList.remove("active");
+    }
+})
+
+
+body.addEventListener("keydown", (evt)=>{
+    if(evt.code === "KeyV"){
+        black[5].classList.add("active");
+        aud22.play();
+        aud22.currentTime = 0;
+    }
+})
+
+body.addEventListener("keyup", (evt)=>{
+    if(evt.code === "KeyV"){
+        black[5].classList.remove("active");
+    }
+})
+
+
+
+body.addEventListener("keydown", (evt)=>{
+    if(evt.code === "KeyW"){
+        black[6].classList.add("active");
+        aud23.play();
+        aud23.currentTime = 0;
+    }
+})
+
+body.addEventListener("keyup", (evt)=>{
+    if(evt.code === "KeyW"){
+        black[6].classList.remove("active");
+    }
+})
+
+
+
+body.addEventListener("keydown", (evt)=>{
+    if(evt.code === "KeyX"){
+        black[7].classList.add("active");
+        aud24.play();
+        aud24.currentTime = 0;
+    }
+})
+
+body.addEventListener("keyup", (evt)=>{
+    if(evt.code === "KeyX"){
+        black[7].classList.remove("active");
+    }
+})
+
+
+
+body.addEventListener("keydown", (evt)=>{
+    if(evt.code === "KeyY"){
+        black[8].classList.add("active");
+        aud25.play();
+        aud25.currentTime = 0;
+    }
+})
+
+body.addEventListener("keyup", (evt)=>{
+    if(evt.code === "KeyY"){
+        black[8].classList.remove("active");
+    }
+})
+
+
+
+body.addEventListener("keydown", (evt)=>{
+    if(evt.code === "KeyZ"){
+        black[9].classList.add("active");
+        aud26.play();
+        aud26.currentTime = 0;
+    }
+})
+
+body.addEventListener("keyup", (evt)=>{
+    if(evt.code === "KeyZ"){
+        black[9].classList.remove("active");
+    }
+})
+
+
+
+body.addEventListener("keydown", (evt)=>{
+    if(evt.code === "Digit1"){
+        black[10].classList.add("active");
+        aud27.play();
+        aud27.currentTime = 0;
+    }
+})
+
+body.addEventListener("keyup", (evt)=>{
+    if(evt.code === "Digit1"){
+        black[10].classList.remove("active");
+    }
+})
+
+
